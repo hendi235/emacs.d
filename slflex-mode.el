ca9d5524d534dc324aa821ede8458ce82b6a1968
hmaulana <h3ndi235@gmail.com>
2015-05-16 16:59:03 +0700

    use regexp to match GACs

--- slflex-mode.el
+++ slflex-mode.el
@@ -17,20 +17,53 @@
       "DIV" "MOD"))
 
 (defvar slflex-functions
-  '("TypeCast"))
+  '("Abort" "AccessStatisticsCounter" "Addition" "AddNumberToList" "AppendAnnoForGroupCall"
+    "AppendAnnouncement" "AppendClassification" "AppendMenuItem" "AppendMenuItemSelCodeList"
+    "AppendValue" "ArrayCopy" "AsciiToChar" "Assignment" "Charge" "DeleteBalObject" "ReadBalance"
+    "Recharge" "ReleaseReservation" "Reserve" "SetSessionCreditLimit" "SetExpirationDate" "SetSessionValues"
+    "BitwiseOps" "BooleanTerm" "CallSubroutine" "ChangeNumberAtPositionInList" "CheckPin" "Condition" "Connect"
+    "CounterCalculateNextDate" "CounterCheckAccumulators" "CounterGetPeriodFactor"
+    "CounterGetSubscriberValue" "CounterInitialize" "CounterRemove" "CounterSetBalances"
+    "CounterSetSubscriberValue" "CreateUniqueId" "CutPrefix" "DecodeString" "DecrementUseCaseCounter"
+    "DeleteNumberFromList" "DeleteSubscription" "DynamicAliasGenAcc" "ExistsVariable"
+    "FindDiscountsForContract" "FindInDataModel" "FindMandatoryPackagesForContract" "FindMnpData"
+    "FindPeriodicForContract" "FindProductForContract" "FindSubscriptionsForContract" "FloatDivision"
+    "FlushNotifications" "Format" "genMsgAddLeafs" "genMsgCreateNewMsg" "genMsgGetEnumValue"
+    "genMsgGetEventType" "genMsgReadEventAttribute" "genMsgSendAndFlush" "GetCalendarField"
+    "GetCellCongestion" "GetCurrentTime" "GetGroupMembership" "GetHandleOfCursor" "GetLogicId"
+    "GetNumberFromList" "GetNumberOfReferencedEntities" "GetOnTouchDateForSubscription"
+    "GetRoamingZone" "GetRoamingZoneByDomain" "GetRoamingZoneByIPAddress" "GetRoamingZoneByMccMnc"
+    "GetSize" "GetSubscriptions" "GetSubscriptionState" "GetSuppliers" "GetTimeId" "GetTimeZoneInfo"
+    "GetValue" "GroupCall" "HttpRequest" "IfEqual" "IfGreater" "IfLess" "IncrementUseCaseCounter"
+    "IndexOf" "InsertNewSubscription" "IntegerDivision" "IsClassificationAvailable" "IsInRange"
+    "IsNumberInList" "IsPerformanceTimerRunning" "IsSubscriptionActive" "IsSubscriptionInfoSet"
+    "IsSubscriptionMandatory" "IsValueAvailable" "LoadSubscriberData" "BalanceIdToName"
+    "BalanceNameToId" "Matrix" "Maximum" "MaximumFromList" "Minimum" "MinimumFromList" "ModifyDate"
+    "ModifyNumberInList" "Modulo" "Multiplication" "Multiplex" "NavigateCursor" "NavigateCursorInList"
+    "NoOperation" "Notify" "NotifyImmediately" "NormalizeNumber" "PatternMatch" "PlayAnnouncement"
+    "PlayMenu" "Prompt" "ReadCIBMessage" "ReadESXMessage" "ReadTicket" "RemoveGenericAccessVariable"
+    "RemoveOptionalGDMAttribute" "RequestNotifyImmediately" "ResetMenuAndAnnos" "SendAtiMessage"
+    "SendAtmMessage" "SendAtsiMessage" "SendCIBMessage" "SendESXMessage" "SendSmsViaMap" "SendSms"
+    "SendUSSDNotification" "SendUSSDPrompt" "SendUSSDResponse" "SetBalance" "SetCallTicketFields"
+    "SetCursorByHandle" "SetLocalPushFields" "SetOnTouchDateForSubscription" "SetPushFields"
+    "SetSubscriptionActive" "SetSubscriptionInfo" "Sort" "StandardVoucherRecharging" "StartPerformanceTimer"
+    "StopPerformanceTimer" "StringCase" "StringConcatenation" "StringLength" "StringReplace"
+    "StringStartsWith" "StringTokenizer" "SubString" "Subtraction" "Switch" "TimeIntervals" "TimeZone"
+    "Trace" "TypeCast" "WasSubscriptionInsertedViaBatch" "WriteConfirmationTicket"))
 
-(defvar slflex-gacs
-  '("theGenericAccess" "CallSupervision"))
+;;(defvar slflex-gacs
+;;  '("theGenericAccess[-_\.A-Za-z0-9]+"))
 
-  (defvar slflex-font-lock-defaults
-    `((
-       ;; ; : , ; { } =>  @ $ = are all special elements
-       ;;(":\\|,\\|;\\|{\\|}\\|=>\\|@\\|$\\|=" . font-lock-keyword-face)
-       ( ,(regexp-opt slflex-keywords 'words) . font-lock-builtin-face)
-       ( ,(regexp-opt slflex-events 'words) . font-lock-constant-face)
-       ( ,(regexp-opt slflex-functions 'words) . font-lock-function-name-face)
-       ( ,(regexp-opt slflex-gacs 'words) . font-lock-preprocessor-face)
-       )))
+(defvar slflex-font-lock-defaults
+  `((
+     ;; ; : , ; { } =>  @ $ = are all special elements
+     ;;(":\\|,\\|;\\|{\\|}\\|=>\\|@\\|$\\|=" . font-lock-keyword-face)
+     ( ,(regexp-opt slflex-keywords 'words) . font-lock-builtin-face)
+     ( ,(regexp-opt slflex-events 'words) . font-lock-constant-face)
+     ( ,(regexp-opt slflex-functions 'words) . font-lock-function-name-face)
+     ;;( ,(regexp-opt slflex-gacs 'words) . font-lock-preprocessor-face)
+     ("theGenericAccess[-_\\.A-Za-z0-9]+" . font-lock-preprocessor-face)
+     )))
 
 (defconst slflex-syntax-table
   (let ((table (make-syntax-table)))
